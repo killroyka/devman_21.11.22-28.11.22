5b40dd3f4250e6838830748980542ea3ad7e1ad9
Тагир Сахабиев <killerbeesexy@gmail.com>
2022-11-23 11:01:29 +0300

    Add model for images

--- devman/app/admin.py
+++ devman/app/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
-from .models import GeoJSON
+from .models import GeoJSON, GeoImage
 
 
-# Register your models here.
+class GeoImageInline(admin.TabularInline):
+    model = GeoImage
 @admin.register(GeoJSON)
-class RecordGeoJSON(admin.ModelAdmin):
+class GeoJSONAdmin(admin.ModelAdmin):
     fields = ("title", "description_short", "description_long", ("lng", "lat"))
+    inlines = [
+        GeoImageInline
+    ]
+    ordering = ('title',)
+    list_display = ("title", "description_short")
+@admin.register(GeoImage)
+class GeoImageAdmin(admin.ModelAdmin):
+    exclude=()
+    list_display = ("geojson", "index")
+
--- devman/app/models.py
+++ devman/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class GeoJSON(models.Model):
     title = models.CharField(max_length=60, verbose_name='Оглавление')
     description_short = models.CharField(max_length=255, verbose_name='Короткое описание')
@@ -11,4 +12,19 @@ class GeoJSON(models.Model):
         verbose_name = 'Точка'
         verbose_name_plural = 'Точки'
 
+    def __str__(self):
+        return self.title
 
+
+class GeoImage(models.Model):
+    img = models.ImageField("Картинка точки", upload_to="GeoImages/")
+    index = models.IntegerField("порядок изображения",
+                                help_text="Введите число кикм по порядку должно быть изображение",
+                                blank=True,
+                                null=True,
+                                unique=True)
+    geojson = models.ForeignKey(GeoJSON, on_delete=models.CASCADE, null=True, blank=True, related_name='images')
+
+    class Meta:
+        verbose_name = 'Картинка локации'
+        verbose_name_plural = 'Картинки локации'
